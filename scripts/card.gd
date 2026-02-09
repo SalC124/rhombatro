bb8a664436ac44a43833301d039b35421edd1157
NateSchmude <nschmude5019@gmail.com>
2026-02-09 15:18:45 -0500

    make sure you actually remove the card from the array or else bad things will happen TODO SAL
    gulp

--- scripts/card.gd
+++ scripts/card.gd
@@ -50,3 +50,7 @@ func _on_area_2d_mouse_entered() -> void:
 
 func _on_area_2d_mouse_exited() -> void:
 	emit_signal("hovered_off", self)
+	
+func discard():
+	$Area2D/CollisionShape2D.disabled = true
+	$CardImage.visible = false
